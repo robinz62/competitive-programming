4fd2c2cd27be5a6d6ec43a7feac4f294d4261211
robinz62 <robinz672@gmail.com>
2020-11-21 01:55:27 -0500

    codeforces edu round 98

---
+++ codeforces/1452D.java
@@ -0,0 +1,167 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static int MOD = 998244353;
+
+    // After writing solution, quick scan for:
+    //   array out of bounds
+    //   special cases e.g. n=1?
+    //
+    // Big numbers arithmetic bugs:
+    //   int overflow
+    //   sorting, or taking max, after MOD
+    void solve() throws IOException {
+        int n = ri();
+        long eChain = 0;
+        long oChain = 0;
+        long[] dp = new long[n+5];
+        dp[0] = 1;
+        long FOUR = modInverseFermat(4, MOD);
+        long TWO = modInverseFermat(2, MOD);
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                eChain = eChain * FOUR % MOD;
+                eChain = (eChain + dp[i-1] * TWO) % MOD;
+                dp[i] = eChain;
+            } else {
+                oChain = oChain * FOUR % MOD;
+                oChain = (oChain + dp[i-1] * TWO) % MOD;
+                dp[i] = oChain;
+            }
+        }
+        pw.println(dp[n]);
+    }
+
+    public static long modPow(long base, long exponent, long m) {
+        long ans = 1;
+        base = base % m;
+        while (exponent > 0) {
+            if ((exponent & 1) == 1) ans = (ans * base) % m;
+            exponent >>= 1;
+            base = (base * base) % m;
+        } 
+        return ans;
+    }
+
+    public static long modInverseFermat(long a, long m) {
+        return modPow(a, m - 2, m);
+    }
+
+    // Template code below
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter pw = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
+    }
+
+    void close() throws IOException {
+        pw.flush();
+        pw.close();
+        br.close();
+    }
+
+    int ri() throws IOException {
+        return Integer.parseInt(br.readLine().trim());
+    }
+
+    long rl() throws IOException {
+        return Long.parseLong(br.readLine().trim());
+    }
+
+    int[] ril(int n) throws IOException {
+        int[] nums = new int[n];
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            int sign = 1;
+            c = br.read();
+            int x = 0;
+            if (c == '-') {
+                sign = -1;
+                c = br.read();
+            }
+            while (c >= '0' && c <= '9') {
+                x = x * 10 + c - '0';
+                c = br.read();
+            }
+            nums[i] = x * sign;
+        }
+        while (c != '\n' && c != -1) c = br.read();
+        return nums;
+    }
+
+    long[] rll(int n) throws IOException {
+        long[] nums = new long[n];
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            int sign = 1;
+            c = br.read();
+            long x = 0;
+            if (c == '-') {
+                sign = -1;
+                c = br.read();
+            }
+            while (c >= '0' && c <= '9') {
+                x = x * 10 + c - '0';
+                c = br.read();
+            }
+            nums[i] = x * sign;
+        }
+        while (c != '\n' && c != -1) c = br.read();
+        return nums;
+    }
+
+    int[] rkil() throws IOException {
+        int sign = 1;
+        int c = br.read();
+        int x = 0;
+        if (c == '-') {
+            sign = -1;
+            c = br.read();
+        }
+        while (c >= '0' && c <= '9') {
+            x = x * 10 + c - '0';
+            c = br.read();
+        }
+        return ril(x);
+    }
+
+    long[] rkll() throws IOException {
+        int sign = 1;
+        int c = br.read();
+        int x = 0;
+        if (c == '-') {
+            sign = -1;
+            c = br.read();
+        }
+        while (c >= '0' && c <= '9') {
+            x = x * 10 + c - '0';
+            c = br.read();
+        }
+        return rll(x);
+    }
+
+    char[] rs() throws IOException {
+        return br.readLine().toCharArray();
+    }
+
+    void sort(int[] A) {
+        Random r = new Random();
+        for (int i = A.length-1; i > 0; i--) {
+            int j = r.nextInt(i+1);
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+        Arrays.sort(A);
+    }
+
+    void printDouble(double d) {
+        pw.printf("%.16f", d);
+    }
+}
