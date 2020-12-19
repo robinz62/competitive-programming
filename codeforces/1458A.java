6c771dfc39ed21ec11e5e357be2d8b83c0ceb7fa
Robin Zhang <robinz672@gmail.com>
2020-12-19 07:13:03 -0500

    codeforces round 691

---
+++ codeforces/1458A.java
@@ -0,0 +1,158 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static int MOD = 1000000007;
+
+    // After writing solution, quick scan for:
+    //   array out of bounds
+    //   special cases e.g. n=1?
+    //
+    // Big numbers arithmetic bugs:
+    //   int overflow
+    //   sorting, or taking max, after MOD
+    void solve() throws IOException {
+        int[] nm = ril(2);
+        int n = nm[0];
+        int m = nm[1];
+        long[] a = rll(n);
+        sort(a);
+        long[] b = rll(m);
+        if (n == 1) {
+            for (long bi : b) {
+                pw.print(a[0] + bi + " ");
+            }
+            pw.println();
+            return;
+        }
+        long g = a[1] - a[0];
+        for (int i = 1; i < n; i++) {
+            g = gcd(g, a[i] - a[i-1]);
+        }
+        for (long bi : b) {
+            pw.print(gcd(a[0] + bi, g) + " ");
+        }
+        pw.println();
+    }
+
+    public static long gcd(long a, long b) {
+        return a == 0 ? b : gcd(b % a, a);
+    }
+
+    // Template code below
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter pw = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
+    }
+
+    void close() throws IOException {
+        pw.flush();
+        pw.close();
+        br.close();
+    }
+
+    int ri() throws IOException {
+        return Integer.parseInt(br.readLine().trim());
+    }
+
+    long rl() throws IOException {
+        return Long.parseLong(br.readLine().trim());
+    }
+
+    int[] ril(int n) throws IOException {
+        int[] nums = new int[n];
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            int sign = 1;
+            c = br.read();
+            int x = 0;
+            if (c == '-') {
+                sign = -1;
+                c = br.read();
+            }
+            while (c >= '0' && c <= '9') {
+                x = x * 10 + c - '0';
+                c = br.read();
+            }
+            nums[i] = x * sign;
+        }
+        while (c != '\n' && c != -1) c = br.read();
+        return nums;
+    }
+
+    long[] rll(int n) throws IOException {
+        long[] nums = new long[n];
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            int sign = 1;
+            c = br.read();
+            long x = 0;
+            if (c == '-') {
+                sign = -1;
+                c = br.read();
+            }
+            while (c >= '0' && c <= '9') {
+                x = x * 10 + c - '0';
+                c = br.read();
+            }
+            nums[i] = x * sign;
+        }
+        while (c != '\n' && c != -1) c = br.read();
+        return nums;
+    }
+
+    int[] rkil() throws IOException {
+        int sign = 1;
+        int c = br.read();
+        int x = 0;
+        if (c == '-') {
+            sign = -1;
+            c = br.read();
+        }
+        while (c >= '0' && c <= '9') {
+            x = x * 10 + c - '0';
+            c = br.read();
+        }
+        return ril(x);
+    }
+
+    long[] rkll() throws IOException {
+        int sign = 1;
+        int c = br.read();
+        int x = 0;
+        if (c == '-') {
+            sign = -1;
+            c = br.read();
+        }
+        while (c >= '0' && c <= '9') {
+            x = x * 10 + c - '0';
+            c = br.read();
+        }
+        return rll(x);
+    }
+
+    char[] rs() throws IOException {
+        return br.readLine().toCharArray();
+    }
+
+    void sort(long[] A) {
+        Random r = new Random();
+        for (int i = A.length-1; i > 0; i--) {
+            int j = r.nextInt(i+1);
+            long temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+        Arrays.sort(A);
+    }
+
+    void printDouble(double d) {
+        pw.printf("%.16f", d);
+    }
+}
